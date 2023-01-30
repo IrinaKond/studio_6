2db6906e53ad56326cadc3626723077fbab78b1a
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2023-01-30 16:00:54 -0600

    quiz classes created

---
+++ src/main/java/Question.java
@@ -0,0 +1,8 @@
+public class Question {
+    String promt;
+    String answer;
+    public Question(String prompt, String answer) {
+        this.promt = prompt;
+        this.answer = answer;
+    }
+}
